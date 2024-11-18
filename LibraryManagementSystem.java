ff1d5f9a158986de7837f2c9a0858c8fc1f04b2c
Faith232005 <demesafathima@gmail.com>
2024-11-18 18:08:10 +0800

    Add files via upload

---
+++ LibraryManagementSystem.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.librarymanagementsystem;
+
+/**
+ *
+ * @author THINKPAD
+ */
+public class LibraryManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
